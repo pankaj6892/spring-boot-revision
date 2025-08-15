2e8c358c0d6e68422a8016ef7c115c69c483edee
PANKAJ SINGH <pankaj6892@gmail.com>
2025-08-15 11:58:21 +0530

    Commit First Spring boot Controller

---
+++ First-spring-boot-controller/src/main/java/com/impankaj/firstspringbootcontroller/FirstSpringBootControllerApplication.java
@@ -0,0 +1,13 @@
+package com.impankaj.firstspringbootcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringBootControllerApplication.class, args);
+    }
+
+}
---
+++ First-spring-boot-controller/src/test/java/com/impankaj/firstspringbootcontroller/FirstSpringBootControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.impankaj.firstspringbootcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringBootControllerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
