ea22074f51b2ae3dc1b84a954a9ffa436a6a8bcc
PANKAJ SINGH <pankaj6892@gmail.com>
2025-08-21 00:28:11 +0530

    Bean LifeCycle Hooks

--- First-spring-boot-controller/src/main/java/com/impankaj/firstspringbootcontroller/FirstSpringBootControllerApplication.java
+++ First-spring-boot-controller/src/main/java/com/impankaj/firstspringbootcontroller/FirstSpringBootControllerApplication.java
@@ -3,14 +3,17 @@ package com.impankaj.firstspringbootcontroller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FirstSpringBootControllerApplication {
 
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(FirstSpringBootControllerApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(FirstSpringBootControllerApplication.class, args);
 
         var heavyResource = context.getBean(HeavyResource.class);
+
+        context.close();
 
 //        var orderService = context.getBean(OrderService.class);
 //        orderService.placeOrder();
--- First-spring-boot-controller/src/main/java/com/impankaj/firstspringbootcontroller/OrderService.java
+++ First-spring-boot-controller/src/main/java/com/impankaj/firstspringbootcontroller/OrderService.java
@@ -1,5 +1,7 @@
 package com.impankaj.firstspringbootcontroller;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,11 @@ public class OrderService {
 
     public OrderService() {}
 
+    @PostConstruct
+    public void init(){
+        System.out.println("OrderService initialized");
+    }
+
 //    @Autowired
     public OrderService(PaymentService paymentService) {
      this.paymentService = paymentService;
@@ -19,5 +26,10 @@ public class OrderService {
 
     public void placeOrder() {
        paymentService.processPayment(50.0);
+    }
+
+    @PreDestroy
+    public void cancelOrder() {
+        System.out.println("Order cancelled");
     }
 }
